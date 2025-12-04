214aa84050250ce7ceb85b3b05584286a036b529
Phan Binh Hao <binhhao612@gmail.com>
2025-12-04 15:12:17 +0700

    Thêm decorator Public để giúp anonymous guest có thể truy cập thông tin listing, metadata

--- packages/server/src/common/guards/jwt-auth.guard.ts
+++ packages/server/src/common/guards/jwt-auth.guard.ts
@@ -1,5 +1,34 @@
-import { Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+
+// @Injectable()
+// export class JwtAuthGuard extends AuthGuard('jwt') {}
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  override canActivate(context: ExecutionContext) {
+    // 1. Dùng Reflector để soi xem Class hoặc Function xử lý request này
+    // có được dán cái nhãn metadata tên là 'isPublic' (IS_PUBLIC_KEY) hay không.
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    // 2. Nếu tìm thấy nhãn Public (isPublic = true)
+    if (isPublic) {
+      return true; // Mở cửa ngay lập tức, không cần hỏi vé (Token).
+    }
+
+    // 3. Nếu không có nhãn Public
+    // Gọi lại logic cũ (super.canActivate) để bắt trình Token ra kiểm tra.
+    return super.canActivate(context);
+    }
+}
